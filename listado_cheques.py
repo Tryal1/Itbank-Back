823bc7fb8b143cd0adb201773f8ebfc3ff32205f
ben-tiki <51ir42824557@campus.economicas.uba.ar>
2022-07-03 00:00:06 -0300

    second commit

--- listado_cheques.py
+++ listado_cheques.py
@@ -1,92 +1,140 @@
+# para leer el archivo csv
 import csv
+# para chequear si el archivo existe
 import os
-from log import StartLog
+# para acceder a la fecha y hora actual
 import datetime
+# para copiar una lista
 import copy
+# para insertar argumentos desde consola
 import sys
+# para registrar las veces que se ejecuta el programa en un txt
+from log import StartLog
 
 
-def FiltrarCheque(nombre_archivo: str, dni: str, salida: str, tipo_cheque: str, estado_cheque = None, rango_fecha_inicio: str = None, rango_fecha_fin: str = None):
-    """ Funcion que filtra cheques en base a un archivo csv y una lista de parametros. """
+PATH = os.path.dirname(os.path.abspath(__file__))
+TIMESTAMP = datetime.datetime.now().strftime("%Y%m%d-%H%M%S")
 
-    path = os.path.dirname(os.path.abspath(__file__))
-    file_path = path + "\\" + nombre_archivo
+def LeerArchivo(nombre_archivo: str):
+    """ Funcion que lee un archivo csv en caso que exista y lo devuelve en forma de lista. """
 
-    timestamp = datetime.datetime.now().strftime("%Y%m%d-%H%M%S")
+    FILE_PATH = PATH + "\\" + nombre_archivo
 
-    if os.path.exists(file_path):
-        
-        rows = []
-        with open(file_path, 'r') as input_csvfile:
+    if os.path.exists(FILE_PATH):
 
+        with open(FILE_PATH, 'r') as input_csvfile:
+
+            rows = []
             reader = csv.DictReader(input_csvfile, delimiter=';')
             for row in reader:
                 rows.append(row)
-        
 
-        filtered_rows = filter(lambda x: x["DNI"] == dni and x["Tipo"] == tipo_cheque, rows)
-        filtered_rows = filter(lambda x: x["Estado"] == estado_cheque, filtered_rows) if estado_cheque else filtered_rows
-        filtered_rows = filter(lambda x: x["FechaPago"] >= rango_fecha_inicio and x["FechaPago"] <= rango_fecha_fin, filtered_rows) if rango_fecha_inicio and rango_fecha_fin else filtered_rows
-
-        number_matches = copy.deepcopy(filtered_rows)
-        number_matches = len(list(number_matches))
-
-        if number_matches > 1:
-
-            print("Error: se encontró más de un cheque con los criterios especificados")
-            StartLog("log.txt", nombre_archivo, dni, salida, "FAILED")
-            return
-        
-        elif number_matches == 0:
-
-            print("Error: no se encontró ningún cheque con los criterios especificados")
-            StartLog("log.txt", nombre_archivo, dni, salida, "FAILED")
-            return
-
-        elif number_matches == 1:
-
-            if salida == "PANTALLA":
-
-                for filtered_row in filtered_rows:
-
-                    print(filtered_row)
-
-            elif salida == "CSV":
-                
-                with open(path+"\\"+f"{dni}_{timestamp}.csv", 'w', newline='') as output_csvfile:
-                    
-                    
-                    fieldnames = ['FechaOrigen', 'FechaPago', 'Valor', 'NumeroCuentaOrigen']
-
-                    def DesiredValues(x): 
-
-                        return {k: v for k, v in x.items() if k in fieldnames}
-                    
-                    filtered_rows = list(map(DesiredValues, filtered_rows))
-
-                    writer = csv.DictWriter(output_csvfile, fieldnames = fieldnames)
-                    writer.writeheader()
-                    writer.writerows(filtered_rows)
-                    
-                    print(f"Se ha creado el archivo {dni}{timestamp}.csv")
-                    StartLog("log.txt", nombre_archivo, dni, salida, "SUCCESS")
+        return rows
 
     else:
 
         print("Error: El archivo no existe. Por favor revisa si exisite el archivo, o bien, está bien escrito el nombre")
+        
 
 
-#FiltrarCheque("test.csv", "11580999", "PANTALLA", "EMITIDO", "APROBADO", ["1620183365", "1620183375"])
+def FiltrarArchivo(rows: list, dni: str, tipo_cheque: str, estado_cheque = None, rango_fecha_inicio: str = None, rango_fecha_fin: str = None):
+    """ Funcion que toma filtros opcionales y devuelve un objeto con los cheques que cumplen con los criterios. """
+
+    filtered_rows = filter(lambda x: x["DNI"] == dni and x["Tipo"] == tipo_cheque, rows)
+    filtered_rows = filter(lambda x: x["Estado"] == estado_cheque, filtered_rows) if estado_cheque else filtered_rows
+    filtered_rows = filter(lambda x: x["FechaPago"] >= rango_fecha_inicio and x["FechaPago"] <= rango_fecha_fin, filtered_rows) if rango_fecha_inicio and rango_fecha_fin else filtered_rows
+
+    return filtered_rows 
+
+
+
+def LevantarErrores(filtered_rows):
+    """ Funcion que levanta errores en caso de que haya registros duplicados o que no se encuentren resultados. """
+
+    number_matches = copy.deepcopy(filtered_rows)
+    number_matches = len(list(number_matches))
+
+    if number_matches > 1:
+
+        print("Error: se encontró más de un cheque con los criterios especificados")
+
+        return 
+        
+    elif number_matches == 0:
+
+        print("Error: no se encontró ningún cheque con los criterios especificados")
+
+        return 
+
+    elif number_matches == 1:
+
+        return True
+
+
+def DevolverCheques(filtered_rows, salida: str, dni: str):
+
+    if salida == "PANTALLA":
+
+        for filtered_row in filtered_rows:
+
+            print(filtered_row)
+
+    elif salida == "CSV":
+        
+        with open(PATH+"\\"+f"{dni}_{TIMESTAMP}.csv", 'w', newline='') as output_csvfile:
+            
+            fieldnames = ['FechaOrigen', 'FechaPago', 'Valor', 'NumeroCuentaOrigen']
+
+            def DesiredValues(x): 
+
+                return {k: v for k, v in x.items() if k in fieldnames}
+            
+            filtered_rows = list(map(DesiredValues, filtered_rows))
+
+            writer = csv.DictWriter(output_csvfile, fieldnames = fieldnames)
+            writer.writeheader()
+            writer.writerows(filtered_rows)
+            
+            print(f"Se ha creado el archivo {dni}{TIMESTAMP}.csv")
+
+
+def Main(nombre_archivo: str, dni: str, salida: str, tipo_cheque: str, estado_cheque = None, rango_fecha_inicio: str = None, rango_fecha_fin: str = None):
+
+    # Leer archivo csv
+    rows = LeerArchivo(nombre_archivo)
+
+    # Filtrar archivo csv
+    filtered_rows = FiltrarArchivo(rows, dni = dni, tipo_cheque = tipo_cheque, estado_cheque = estado_cheque, rango_fecha_inicio = rango_fecha_inicio, rango_fecha_fin = rango_fecha_fin)
+
+    # Levantar errores
+    status = LevantarErrores(rows, filtered_rows)
+
+    # Devolver cheques
+    DevolverCheques(filtered_rows, salida = salida, dni = dni) if status else None
+
+    # Registro de fin y status del programa
+    StartLog("log.txt", nombre_archivo, dni, salida, "SUCCESS", len(sys.argv), status = "SUCCESS" if status else "FAIL")
+
+
+# para probar el código
+# python listado_cheques.py test.csv 11580999 PANTALLA EMITIDO APROBADO 1620183365 1620183375 -> Utilizando filtros opcionales de estado y rango de fecha (caso exitoso)
+# python listado_cheques.py test.csv 11580999 PANTALLA EMITIDO APROBADO -> Utilizando filtro opcional de estado  y salida = CSV (caso exitoso)
+# python listado_cheques.py test.csv 11580999 PANTALLA EMITIDO -> Sin utilizar los filtros opcionales (caso exitoso)
+
 if __name__ == "__main__":
 
-    if len (sys.argv) == 7:
+    if len (sys.argv) == 8:
 
-        FiltrarCheque(sys.argv[1], sys.argv[2], sys.argv[3], sys.argv[4], sys.argv[5], sys.argv[6], sys.argv[7])
+        Main(nombre_archivo = sys.argv[1], dni = sys.argv[2], salida = sys.argv[3], tipo_cheque = sys.argv[4], estado_cheque = sys.argv[5], rango_fecha_inicio = sys.argv[6], rango_fecha_fin = sys.argv[7])
 
     elif len (sys.argv) == 6:
 
-        FiltrarCheque(sys.argv[1], sys.argv[2], sys.argv[3], sys.argv[4], sys.argv[5], sys.argv[6])
+        Main(nombre_archivo = sys.argv[1], dni = sys.argv[2], salida = sys.argv[3], tipo_cheque = sys.argv[4], estado_cheque = sys.argv[5])
 
     elif len (sys.argv) == 5:
 
-        FiltrarCheque(sys.argv[1], sys.argv[2], sys.argv[3], sys.argv[4])
+         Main(nombre_archivo = sys.argv[1], dni = sys.argv[2], salida = sys.argv[3], tipo_cheque = sys.argv[4])
+    
+    else:
+
+        print("Error: El número de argumentos es incorrecto")
--- log.py
+++ log.py
@@ -1,7 +1,7 @@
 import datetime
 import os
 
-def StartLog(nombre_log, nombre_archivo, dni, salida, status):
+def StartLog(nombre_log, nombre_archivo, dni, salida, cantidad_argumentos, status):
 	""" Abre el archivo de log indicado. Devuelve el archivo abierto."""
 
 	path = os.path.dirname(os.path.abspath(__file__))
@@ -12,7 +12,7 @@ def StartLog(nombre_log, nombre_archivo, dni, salida, status):
 	hora_actual = str(datetime.datetime.now())
 
 	archivo_log.write(f"{hora_actual}: IniScio de registro de actividad\n")
-	archivo_log.write(f"{hora_actual} nombre del archivo: {nombre_archivo}  dni: {dni} - salida: {salida} - status: {status} \n")
+	archivo_log.write(f"{hora_actual} nombre del archivo: {nombre_archivo}  dni: {dni} - salida: {salida} - status: {status} - cantidad de argumentos: {cantidad_argumentos} status: {status} \n")
 	archivo_log.write(f"{hora_actual}: Fin de registro de actividad\n")
 
 	archivo_log.close()
